56321603b001a0e58b0d74c0cb5312a72af697c5
Tran Vo Son Tung <sontungexpt@gmail.com>
2024-08-06 08:52:01 +0700

    update to remote

---
+++ README.md
@@ -0,0 +1,103 @@
+The built-in function help you improve the resizing behavior of neovim.
+
+## Installation
+
+```lua
+
+    --lazy
+    {
+        lazy = true,
+        "sontungexpt/smart-resizing.nvim",
+    }
+
+```
+
+## Features
+
+🚀 **Enhance Your Window Management with Smart Resizing and Centering!**
+
+## 🖼️ **Smart Window Resizing:**
+
+- **Left Side Window:**
+  - Press `Alt + L` to **expand** the width and give your content more room.
+- **Right Side Window:**
+  - Press `Alt + H` to **shrink** the width and make more space for other windows.
+- **Top Side Window:**
+  - Press `Alt + J` to **increase** the height and view more of your content.
+- **Bottom Side Window:**
+  - Press `Alt + K` to **decrease** the height and tidy up your workspace.
+- 🔄 **Seamlessly adapts to the opposite sides as well!**
+
+### 📍 **Center Your Focus:**
+
+- **When Managing Multiple Windows:**
+  - Use `Alt + H` or `Alt + L` to adjust the width of the current window, centering it perfectly on your screen.
+  - If resizing is no longer possible, the window will smoothly slide to the rightmost position.
+- **For Height Adjustments:**
+  - Use `Alt + J` or `Alt + K` to resize and center the height.
+  - If the height cannot be resized further, the window will glide to the bottommost position.
+
+✨ **Elevate your productivity with effortless window management!** Experience a more organized and efficient workspace with just a few keystrokes. 🚀
+
+## Usage
+
+Because this plugin is just contain a builtin function, you need to map it to a keybinding.
+like this:
+
+```lua
+    -- recommended to use this function since it has a speed up behavior when you hold the keybinding
+    -- You should map the keybinding with the modifier key + h/j/k/l
+
+    map("n", "<A-h>", function() require("smart-resizing").adjust_current_win_width(1, 1) end)
+    map("n", "<A-l>", function() require("smart-resizing").adjust_current_win_width(1, 2) end)
+    map("n", "<A-j>", function() require("smart-resizing").adjust_current_win_height(1, 1) end)
+    map("n", "<A-k>", function() require("smart-resizing").adjust_current_win_height(1, 2) end)
+```
+
+## Built-in function
+
+### Action:
+
+- 1: decrease
+- 2: increase
+
+You can access this enum by `require("smart-resizing").Action`
+
+### Dimension:
+
+- 1: height
+- 2: width
+
+You can access this enum by `require("smart-resizing").Dimension`
+
+### Functions:
+
+- `adjust_current_win_width(step, action)`: adjust the width of the current window (speed up behavior when you hold the keybinding)
+
+  - `adjust_current_win_width(1, 1)`: decrease the width of the current window by 1
+  - `adjust_current_win_width(1, 2)`: increase the width of the current window by 1
+
+- `adjust_current_win_height(step, action)`: adjust the height of the current window (speed up behavior when you hold the keybinding)
+
+  - `adjust_current_win_height(1, 1)`: decrease the height of the current window by 1
+  - `adjust_current_win_height(1, 2)`: increase the height of the current window by 1
+
+- `increase_current_win_size(step, dimension)`: increase the size of the current window
+
+  - `increase_current_win_size(1, 1)`: increase the height of the current window by 1
+  - `increase_current_win_size(1, 2)`: increase the width of the current window by 1
+
+- `decrease_current_win_size(step, dimension)`: decrease the size of the current window
+
+  - `decrease_current_win_size(1, 1)`: decrease the height of the current window by 1
+  - `decrease_current_win_size(1, 2)`: decrease the width of the current window by 1
+
+- `increase_current_win_width(step)`: increase the width of the current window by 1
+
+- `decrease_current_win_width(step)`: decrease the width of the current window by 1
+
+- `increase_current_win_height(step)`: increase the height of the current window by 1
+
+- `decrease_current_win_height(step)`: decrease the height of the current window by 1
+
+## License
---
+++ lua/smart-resizing.lua
@@ -0,0 +1,235 @@
+local uv = vim.uv or vim.loop
+local fn = vim.fn
+local api = vim.api
+local o = vim.o
+local fn_winnr = fn.winnr
+
+local M = {}
+
+---@alias DimensionEnum number
+---| 1 # HEIGHT
+---| 2 # WIDTH
+---
+
+---@type table<string, DimensionEnum>
+local Dimension = {
+	HEIGHT = 1,
+	WIDTH = 2,
+}
+M.Dimension = Dimension
+
+---@alias ActionEnum number
+---| 1 # INCREASE
+---| 2 # DECREASE
+
+---@type table<string, ActionEnum>
+local Action = {
+	INCREASE = 1,
+	DECREASE = 2,
+}
+
+M.Action = Action
+
+--- Create a speedup callback
+--- @param cb fun(step: number, action: ActionEnum) The callback function
+--- @param speedup_threshold? uinteger The threshold of the speedup
+--- @param adjustment_factor? uinteger The adjustment factor of the speedup
+--- @return fun(step: number, action: ActionEnum) The adjust size callback
+local function create_speedup_callback(cb, speedup_threshold, adjustment_factor)
+	---@diagnostic disable: undefined-field
+	local timer = uv.new_timer()
+	local last_call_time = -1
+	local is_speedup = false
+
+	speedup_threshold = speedup_threshold or 600
+	adjustment_factor = adjustment_factor or 2
+
+	--- @param step number The step of increasing
+	--- @param action ActionEnum The action of the window resizing
+	return function(step, action)
+		if is_speedup then
+			cb(math.ceil((uv.now() - last_call_time) / 1000 + adjustment_factor), action)
+
+			-- stop speedup after timeoutlen of inactivity
+			timer:stop()
+
+			timer:start(o.timeoutlen, 0, function()
+				timer:stop()
+				is_speedup = false
+				last_call_time = -1
+			end)
+		else
+			if last_call_time < 0 then
+				last_call_time = uv.now()
+			end
+			cb(step, action)
+			if uv.now() - last_call_time > speedup_threshold then
+				is_speedup = true
+			end
+		end
+	end
+end
+
+--- Get the window id of the direction or the current window
+local function winid(direction)
+	return direction and fn.win_getid(fn_winnr(direction)) or api.nvim_get_current_win()
+end
+
+---Get the middle position of the vim
+---@param dimension DimensionEnum The dimension of the window
+---@return number The middle position of the vim
+local function middle_vim_position(dimension)
+	if dimension == Dimension.WIDTH then
+		return o.columns / 2
+	else
+		return (
+			o.lines
+			- o.cmdheight
+			- (o.laststatus ~= 0 and 1 or 0)
+			- (o.showtabline ~= 0 and #api.nvim_list_tabpages() > 1 and 1 or 0)
+		) / 2
+	end
+end
+
+---Get the window properties
+---@param dimension DimensionEnum The dimension of the window
+---@param step uinteger The step of increasing
+local function get_window_properties(dimension, step)
+	local width_dimension = dimension == Dimension.WIDTH
+	local winnr = fn_winnr()
+	local set_fn, get_fn, winmin, primary_direction = nil, nil, nil, nil
+
+	if width_dimension then
+		primary_direction = winnr == fn_winnr("l") and -step or winnr == fn_winnr("h") and step or nil
+		set_fn = api.nvim_win_set_width
+		get_fn = api.nvim_win_get_width
+		winmin = o.winminwidth
+	else
+		primary_direction = winnr == fn_winnr("j") and -step or winnr == fn_winnr("k") and step or nil
+		set_fn = api.nvim_win_set_height
+		get_fn = api.nvim_win_get_height
+		winmin = o.winminheight
+	end
+
+	local win_size = get_fn(0)
+
+	return {
+		width_dimension = width_dimension,
+		primary_direction = primary_direction, -- The primary direction of the window resizing in increasing if decreasing then change this prop to negative
+		set_fn = set_fn,
+		get_fn = get_fn,
+		winmin = winmin,
+		win_size = win_size,
+		middle_position = api.nvim_win_get_position(0)[dimension] + win_size / 2,
+	}
+end
+
+---Adjust the window dimension smartly
+---@param step uinteger The step of increasing
+---@param dimension DimensionEnum The dimension of the window
+M.increase_current_win_size = function(step, dimension)
+	vim.schedule(function()
+		local props = get_window_properties(dimension, step)
+		local get_fn = props.get_fn
+		local set_fn = props.set_fn
+		local win_size = props.win_size
+
+		if props.primary_direction then
+			set_fn(0, win_size + props.primary_direction)
+		elseif props.middle_position < middle_vim_position(dimension) then
+			set_fn(0, win_size + step)
+		else
+			local hork = props.width_dimension and "h" or "k"
+			local idx = 1
+			local last_id = -1
+			local target_id = winid(hork)
+			local winmin = props.winmin
+
+			local dimensions = {
+				{ id = target_id, size = get_fn(target_id) },
+			}
+
+			while dimensions[idx].size - step <= winmin do
+				if last_id == target_id then
+					set_fn(0, win_size + step)
+					break
+				else
+					last_id = target_id
+					idx = idx + 1
+					target_id = winid(hork)
+					dimensions[idx] = { id = target_id, size = get_fn(target_id) }
+				end
+			end
+
+			for i = idx, 1, -1 do
+				local adjust_size = step * idx
+				local new_size = dimensions[i].size - adjust_size
+				if new_size < winmin then
+					adjust_size = dimensions[i].size - winmin
+					new_size = winmin
+				end
+				set_fn(dimensions[i].id, new_size)
+				if i > 1 then
+					dimensions[i - 1].size = dimensions[i - 1].size + adjust_size
+				end
+			end
+		end
+	end)
+end
+
+--- Decrease the window dimension smartly
+--- @param step uinteger The step of decreasing
+--- @param dimension DimensionEnum The dimension of the window
+M.decrease_current_win_size = function(step, dimension)
+	local props = get_window_properties(dimension, step)
+	local get_fn = props.get_fn
+	local set_fn = props.set_fn
+	local win_size = props.win_size
+	local winmin = props.winmin
+
+	local lorj = props.width_dimension and "l" or "j"
+
+	if props.primary_direction then
+		set_fn(0, win_size - props.primary_direction)
+	elseif win_size > winmin and props.middle_position > middle_vim_position(dimension) then
+		set_fn(0, win_size - step)
+	elseif fn.winwidth(fn.winnr(lorj)) > winmin or win_size > winmin then
+		local hork = props.width_dimension and "h" or "k"
+		local target_id = winid(hork)
+		set_fn(target_id, get_fn(target_id) + step)
+	end
+end
+
+M.increase_current_win_width = function(step)
+	M.increase_current_win_size(Dimension.WIDTH, step)
+end
+
+M.decrease_current_win_width = function(step)
+	M.decrease_current_win_size(Dimension.WIDTH, step)
+end
+
+M.increase_current_win_height = function(step)
+	M.increase_current_win_size(Dimension.HEIGHT, step)
+end
+
+M.decrease_current_win_height = function(step)
+	M.decrease_current_win_size(Dimension.HEIGHT, step)
+end
+
+M.adjust_current_win_width = create_speedup_callback(function(step, action)
+	if action == Action.INCREASE then
+		M.increase_current_win_size(Dimension.WIDTH, step)
+	else
+		M.decrease_current_win_size(Dimension.WIDTH, step)
+	end
+end)
+
+M.adjust_current_win_height = create_speedup_callback(function(step, action)
+	if action == Action.DECREASE then
+		M.increase_current_win_size(Dimension.HEIGHT, step)
+	else
+		M.decrease_current_win_size(Dimension.HEIGHT, step)
+	end
+end)
+
+return M
